fb3c13757fd9ce7f38aba82082ba688fac0acc98
sohee00314 <sohee00314@gmail.com>
2025-09-17 16:50:35 +0900

    ✅ Test : 크롤링 테스트(상품목록 10페이지까지)

--- crawling-service/src/main/java/com/example/crawlingservice/service/CrawlingService.java
+++ crawling-service/src/main/java/com/example/crawlingservice/service/CrawlingService.java
@@ -98,8 +98,8 @@ public class CrawlingService {
                     log.info("현재 페이지에서 {}개 상품 수집, 총 {}개", pageItems.size(), allProducts.size());
 
                     //다음 페이지로 넘어가는 ui가져오기
-                    List<WebElement> nextButtons = driver.findElements(By.cssSelector("div.num_nav_wrap a.num.now_on + a.num, div.num_nav_wrap a.edge_nav.nav_next, div.num_nav_wrap a.nav_next"));
-//                    List<WebElement> nextButtons = driver.findElements(By.cssSelector("div.num_nav_wrap a.num.now_on + a.num")); // 테스트용
+//                    List<WebElement> nextButtons = driver.findElements(By.cssSelector("div.num_nav_wrap a.num.now_on + a.num, div.num_nav_wrap a.edge_nav.nav_next, div.num_nav_wrap a.nav_next"));
+                    List<WebElement> nextButtons = driver.findElements(By.cssSelector("div.num_nav_wrap a.num.now_on + a.num")); // 테스트용
                     if (nextButtons.isEmpty()) {
                         // 다음 페이지 버튼이 없으면 마지막 페이지
                         log.info("마지막 페이지 도달");
