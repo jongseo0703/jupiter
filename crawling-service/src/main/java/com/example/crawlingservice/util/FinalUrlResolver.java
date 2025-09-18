d6183452cd02f973781dab406c8ce244148b472f
sohee00314 <sohee00314@gmail.com>
2025-09-18 14:34:45 +0900

    🚧 WIP : 로그인 페이지 없는 최종구매사이트 링크 추출하는 클래스 구현 중

---
+++ crawling-service/src/main/java/com/example/crawlingservice/util/FinalUrlResolver.java
@@ -0,0 +1,9 @@
+package com.example.crawlingservice.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class FinalUrlResolver {
+}
