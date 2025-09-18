536c7c69319eb3d5009cf09a6e804aa73da8b858
jongseo0703 <jongseo0703@naver.com>
2025-09-18 15:25:22 +0900

    ✨ Feat : 알림설정 로직을 정의하는 Repository 생성

---
+++ auth-service/src/main/java/com/example/authservice/notification/repository/NotificationSettingsRepository.java
@@ -0,0 +1,18 @@
+package com.example.authservice.notification.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.authservice.notification.entity.NotificationSettings;
+
+@Repository
+public interface NotificationSettingsRepository extends JpaRepository<NotificationSettings, Long> {
+
+  Optional<NotificationSettings> findByUserId(Long userId);
+
+  boolean existsByUserId(Long userId);
+
+  void deleteByUserId(Long userId);
+}
