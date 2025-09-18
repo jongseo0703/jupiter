e8cc21b255db52d484d4db0c37ecb0522d7ab48c
sohee00314 <sohee00314@gmail.com>
2025-09-18 15:07:52 +0900

    🚧 WIP : 최종 구매사이트 URL를 추출할 메서드 구현중

--- crawling-service/src/main/java/com/example/crawlingservice/util/FinalUrlResolver.java
+++ crawling-service/src/main/java/com/example/crawlingservice/util/FinalUrlResolver.java
@@ -1,9 +1,21 @@
 package com.example.crawlingservice.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class FinalUrlResolver {
+    public String resolve(String url, WebDriver driver) {
+        //최종 URL 초기화
+        String finalUrl = url;
+        //제품 상세페이지로 돌아가기 위한 탭 저장
+        String originalTab =  driver.getCurrentUrl();
+        //새로운 탭을 저장할 변수 초기화
+        String newTab = null;
+
+        //최종 URL 반환
+        return  finalUrl;
+    }
 }
