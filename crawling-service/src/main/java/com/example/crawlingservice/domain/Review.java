2f664c7cc7895e4e9045ff95c0f41d7eb75deab6
sohee00314 <sohee00314@gmail.com>
2025-09-22 09:20:52 +0900

    ✨ Feat : 데이터베이스의 review 테이블과 매핑할 엔티티 클래스

---
+++ crawling-service/src/main/java/com/example/crawlingservice/domain/Review.java
@@ -0,0 +1,22 @@
+package com.example.crawlingservice.domain;
+
+import lombok.Data;
+
+
+@Data
+public class Review {
+    //리뷰의 고유 키값
+    private int reviewId;
+    //작성자
+    private String writer;
+    //별점
+    private int rating;
+    //제목
+    private String title;
+    //내용
+    private String comment;
+    //작성일
+    private String reviewDate;
+    //상품
+    private Product product;
+}
