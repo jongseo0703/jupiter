08004527ca89e3d5eafffaab6b06c97c13e880f7
sohee00314 <sohee00314@gmail.com>
2025-09-22 09:06:03 +0900

    ✨ Feat : 데이터베이스의 product 테이블과 매핑할 엔티티 클래스

---
+++ crawling-service/src/main/java/com/example/crawlingservice/domain/Product.java
@@ -0,0 +1,22 @@
+package com.example.crawlingservice.domain;
+
+public class Product {
+    //상품의 고유 키값
+    private int productId;
+    //상품명
+    private String productName;
+    //상세설명
+    private String description;
+    //브랜드
+    private String brand;
+    //도수
+    private int alcohol_percentage;
+    //상품 이미지
+    private String url;
+    //용량
+    private double volume;
+    //하위 카테고리
+    private SubCategory subCategory;
+
+
+}
