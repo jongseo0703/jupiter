ebd4cf10d9222da047f5e1dc2be6a418722ad24e
sohee00314 <sohee00314@gmail.com>
2025-09-22 09:13:40 +0900

    ✨ Feat : 데이터베이스의 price 테이블과 매핑할 엔티티 클래스

---
+++ crawling-service/src/main/java/com/example/crawlingservice/domain/Price.java
@@ -0,0 +1,15 @@
+package com.example.crawlingservice.domain;
+
+import lombok.Data;
+
+@Data
+public class Price {
+    //가격의 고유 키값
+    private int priceId;
+    //가격
+    private int price;
+    //배송비
+    private int deliveryFee;
+    //상점
+    private Shop shop;
+}
