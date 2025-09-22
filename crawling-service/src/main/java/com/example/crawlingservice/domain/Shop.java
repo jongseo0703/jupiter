b29b09af3c503a93c80097e7207dede19d1c5893
sohee00314 <sohee00314@gmail.com>
2025-09-22 09:09:54 +0900

    ✨ Feat : 데이터베이스의 shop 테이블과 매핑할 엔티티 클래스

---
+++ crawling-service/src/main/java/com/example/crawlingservice/domain/Shop.java
@@ -0,0 +1,14 @@
+package com.example.crawlingservice.domain;
+
+public class Shop {
+    //구매 상점의 고유 키값
+    private int shopId;
+    //구매 상점명
+    private String shopName;
+    //구매 상점 링크
+    private String link;
+    //구매 상점 로고
+    private String logoIcon;
+    //상품
+    private Product product;
+}
