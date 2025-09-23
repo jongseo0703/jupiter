0d9739928db746375720a9ef35c7bd2b83e411c1
sohee00314 <sohee00314@gmail.com>
2025-09-23 17:22:48 +0900

    ✨ Feat : 자동으로 topcategory_id 자동 증가 설정과 top_name으로 조회 쿼리문 추가

--- crawling-service/src/main/java/com/example/crawlingservice/db/TopCategoryMapper.java
+++ crawling-service/src/main/java/com/example/crawlingservice/db/TopCategoryMapper.java
@@ -1,7 +1,10 @@
 package com.example.crawlingservice.db;
 
+import com.example.crawlingservice.domain.TopCategory;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,10 +15,19 @@ import java.util.List;
 public interface TopCategoryMapper {
     /**
      * 상위 카테고리 이름을 받아 데이터베이스에 저장
-     * @param topName 상위 카테고리
+     * @param topCategory 상위 카테고리
      */
     @Insert("insert into topcategory(top_name) values (#{topName})")
-    void insert(String topName);
+    @Options(useGeneratedKeys = true,keyProperty = "topcategory_id")
+    void insert(TopCategory topCategory);
+
+    /**
+     * 상위 카테고리명을 받아 조회
+     * @param topName 조회할 상위카테고리명
+     * @return topCategory 반환
+     */
+    @Select("select * from topcategory where top_name=#{topName}")
+    TopCategory getTopCategory(String topName);
 
 
 }
