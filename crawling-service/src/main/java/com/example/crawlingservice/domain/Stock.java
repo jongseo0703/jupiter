6a664f4529786df04fba1317bda65e3b2541032a
sohee00314 <sohee00314@gmail.com>
2025-09-24 14:00:36 +0900

    ✨ Feat : 재고 테이블 엔티티 클래스 생성

---
+++ crawling-service/src/main/java/com/example/crawlingservice/domain/Stock.java
@@ -0,0 +1,10 @@
+package com.example.crawlingservice.domain;
+
+public class Stock {
+    //재고 고유 아이디
+    private int stockId;
+    //존재 여부
+    private boolean isAvailable;
+    //상품 정보
+    private Product product;
+}
