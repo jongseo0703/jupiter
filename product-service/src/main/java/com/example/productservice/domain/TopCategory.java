6d80f53960a455ed786e6471d6150f1e0d6455c4
sohee00314 <sohee00314@gmail.com>
2025-09-25 16:03:03 +0900

    ✨ Feat : TopCategory 엔티티 클래스 생성

---
+++ product-service/src/main/java/com/example/productservice/domain/TopCategory.java
@@ -0,0 +1,25 @@
+package com.example.productservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * 상위 카테고리 테이블의 엔티티 클래스
+ * <ui>
+ *     <li>topcategoryId : 상위 카테고리의 고유 아이디</li>
+ *     <li>topName : 상위 카테고리명</li>
+ * </ui>
+ */
+@Data
+@Entity
+@Table(name = "topcategory")
+public class TopCategory {
+    @Id
+    @Column(name = "topcategory_id")
+    private int topcategoryId;
+
+    private String topName;
+}
