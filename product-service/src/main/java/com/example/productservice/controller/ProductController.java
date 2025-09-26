ec6a57c7b3aa4cc9989201063b2925385dbaea9f
sohee00314 <sohee00314@gmail.com>
2025-09-26 14:28:59 +0900

    🐛 Fix : 라우팅 변경으로 인한 404 에러 수정

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
