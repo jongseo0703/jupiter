13bbb6f4139b87bbfb59a7fd8c3dfb48c6e978a0
sohee00314 <sohee00314@gmail.com>
2025-09-26 11:04:32 +0900

    ✨ Feat : 메인 페이지 상품 목록 조회 컨트롤러 구현

---
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.example.productservice.controller;
+
+import com.example.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    /**
+     * 메인 페이지 상품정보 조회
+     * @return 상품 목록
+     */
+    @GetMapping("/main")
+    public ResponseEntity<?> getMain() {
+        List<Map<String, Object>> mainProducts = productService.mainPageProducts();
+        return ResponseEntity.ok().body(mainProducts);
+    }
+}
