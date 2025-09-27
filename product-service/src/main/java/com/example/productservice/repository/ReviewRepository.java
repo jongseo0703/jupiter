4cc66f582741ec03505b2655f0d815ba06ef4604
sohee00314 <sohee00314@gmail.com>
2025-09-27 23:08:18 +0900

    ✨ Feat : 특정 상품 전체 리뷰 조회 JPA 로직 구현

--- product-service/src/main/java/com/example/productservice/repository/ReviewRepository.java
+++ product-service/src/main/java/com/example/productservice/repository/ReviewRepository.java
@@ -4,6 +4,8 @@ import com.example.productservice.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review,Integer> {
 
     /**
@@ -15,4 +17,15 @@ public interface ReviewRepository extends JpaRepository<Review,Integer> {
             "JOIN ProductShop ps ON r.productShop.productShopId = ps.productShopId " +
             "WHERE ps.product.productId = :productId")
     Double findAvgRatingByProductId(Integer productId);
+
+    /**
+     * 특정 상품의 전체 리뷰 정보
+     * @param productId 상품 아이디
+     * @return 전체 리뷰 정보 및 상점 정보
+     */
+    @Query("select r.reviewId ,r.writer ,r.rating,r.reviewDate,r.title,r.comment,s.shopName,s.logoIcon from Review r " +
+            "inner join ProductShop ps on ps.productShopId = r.productShop.productShopId " +
+            "inner join Shop s on s.shopId =ps.shop.shopId " +
+            "inner join Product p on p.productId =ps.product.productId where p.productId = :productId")
+    List<Object[]> findReviewByProductId(Integer productId);
 }
