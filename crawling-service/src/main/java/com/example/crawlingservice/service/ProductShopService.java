a6d96266cae10adfff244d2720e92e1712a7fee5
sohee00314 <sohee00314@gmail.com>
2025-10-01 01:34:35 +0900

    ✨ Feat : 추출된 상점명 기반 유효 상점 목록 업데이트 로직 구현

--- crawling-service/src/main/java/com/example/crawlingservice/service/ProductShopService.java
+++ crawling-service/src/main/java/com/example/crawlingservice/service/ProductShopService.java
@@ -1,5 +1,6 @@
 package com.example.crawlingservice.service;
 
+import com.example.crawlingservice.db.ProductMapper;
 import com.example.crawlingservice.db.ProductShopMapper;
 import com.example.crawlingservice.domain.Product;
 import com.example.crawlingservice.domain.ProductShop;
@@ -8,6 +9,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 상품_상점 정보 저장하는 클래스
@@ -18,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ProductShopService {
     private final ProductShopMapper productShopMapper;
+    private final ProductMapper productMapper;
 
     /**
      * 상품_상점 정보를 저장하는 메서드<br>
@@ -61,4 +67,31 @@ public class ProductShopService {
         return newProductShop;
     }
 
+    /**
+     * 특정 상품의 모든 상점 유효성을 크롤링 결과와 한 번에 비교하여 업데이트
+     * @param productId 상품 아이디
+     * @param crawledShopNames 크롤링으로 추출한 상점명 목록
+     */
+    public void checkShops(int productId, Set<String> crawledShopNames) {
+        // DB에 저장된 해당 상품의 모든 상품_상점 정보 조회
+        List<ProductShop> dbProductShops = productShopMapper.selectAllByProductId(productId);
+
+        // 크롤링한 상점명 목록
+        Set<String> crawledShopSet = new HashSet<>(crawledShopNames);
+
+        //  데이터베이스의 각 상점을 크롤링 결과와 비교
+        for (ProductShop productShop : dbProductShops) {
+            String dbShopName = productShop.getShop().getShopName();
+            //상점명 비교
+            boolean shouldBeAvailable = crawledShopSet.contains(dbShopName);
+            //상점 유효성 조회
+            boolean currentStatus = productShop.isAvailable();
+
+            // 상태가 다를 때만 업데이트
+            if (currentStatus != shouldBeAvailable) {
+                productShopMapper.updateIsAvailable(shouldBeAvailable, productShop.getProductShopId());
+                log.debug("상점의 유효성이 변경되었습니다");
+            }
+        }
+    }
 }
