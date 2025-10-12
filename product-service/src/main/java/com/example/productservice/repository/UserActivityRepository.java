d2e685f6f5d4a3555618347184687cff454796da
heeezni <hjin3456@naver.com>
2025-10-12 23:11:04 +0900

    ✨ Feat : 추천 시스템을 위한 Repository 추가

---
+++ product-service/src/main/java/com/example/productservice/repository/UserActivityRepository.java
@@ -0,0 +1,13 @@
+package com.example.productservice.repository;
+
+import com.example.productservice.domain.UserActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserActivityRepository extends JpaRepository<UserActivity, Long> {
+    List<UserActivity> findByUserId(Long userId);
+    List<UserActivity> findByUserIdAndProductId(Long userId, Integer productId);
+}
