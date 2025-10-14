05c3899c6e277d09dabb59d42e3ec1c4bbfd4b2a
sohee00314 <sohee00314@gmail.com>
2025-10-15 00:23:19 +0900

    ✨ Feat : 특정 상품의 리뷰 내용을 분석한는 서비스 구현

--- product-service/src/main/java/com/example/productservice/service/ReviewAnalyzeSerivce.java
+++ product-service/src/main/java/com/example/productservice/service/ReviewAnalyzeSerivce.java
@@ -1,10 +1,13 @@
 package com.example.productservice.service;
 
 import com.example.productservice.repository.ReviewRepository;
+import com.example.productservice.util.LoadKeywordAnalyzer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,11 +23,14 @@ public class ReviewAnalyzeSerivce {
     /**
      * 특정 상품의 리뷰 내용을 분석하여 점수를 부여하는 메서드
      * @param productId 상품 아이디
-     * @return 맛,바디감,가성비,만족도 그래프의 점수 반환
+     * @return 맛,바디감,가성비,만족도 점수표
      */
-    public Map<String,Map<String,Integer>> reviewScore(int productId){
+    public Map<String, Map<String, Integer>> reviewScore(int productId){
         //특정 상품의 리뷰 내용 목록
         List<String>commentList= reviewRepository.findCommentsByProductId(productId);
-        return  null;
+
+        //리뷰내용 분석하여 점수화 하는 클래스 초기화
+        LoadKeywordAnalyzer loadKeywordAnalyzer = new LoadKeywordAnalyzer();
+        return loadKeywordAnalyzer.analyzeReview(commentList);
     }
 }
