4cd2ee5cddf4470fc3df55ac26a1a9cd48f39c3c
sohee00314 <sohee00314@gmail.com>
2025-10-15 11:08:27 +0900

    🔥 Remove : 사용하지 않는 주입 제거

--- product-service/src/main/java/com/example/productservice/controller/ReviewController.java
+++ product-service/src/main/java/com/example/productservice/controller/ReviewController.java
@@ -2,7 +2,6 @@ package com.example.productservice.controller;
 
 import com.example.productservice.service.ReviewAnalyzeSerivce;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
