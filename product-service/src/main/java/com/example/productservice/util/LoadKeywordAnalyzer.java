5b8c18d395563d2bfc04dd5b14436a302848a2bf
sohee00314 <sohee00314@gmail.com>
2025-10-15 17:34:01 +0900

    ✨ Feat : 자주 쓰는 감탄사 제외

--- product-service/src/main/java/com/example/productservice/util/LoadKeywordAnalyzer.java
+++ product-service/src/main/java/com/example/productservice/util/LoadKeywordAnalyzer.java
@@ -19,6 +19,8 @@ public class LoadKeywordAnalyzer {
     private final Komoran komoran;
     private final Map<String, List<String>> keywordMorphemeCache = new ConcurrentHashMap<>();
 
+    //일반적이 감탄사 키워드
+    private static final Set<String> STOPWORDS = Set.of("잘", "매우", "진짜", "정말");
     // 제외 키워드
     private static final List<String> SERVICE_KEYWORDS = Arrays.asList(
         "배송", "택배", "직원", "서비스", "일처리", "응대",
@@ -56,10 +58,9 @@ public class LoadKeywordAnalyzer {
      * 바디감 키워드
      */
     private void loadBodyKeywords() {
-        bodyKeywords.put("부드러워요", Arrays.asList("부드", "목넘김 좋다","순하다", "매끄럽다", "술술 넘어간다", "거슬림이 없다"
-                            ,"마일드","편안","매끈","크리미","여운"));
-        bodyKeywords.put("깔끔해요", Arrays.asList("무난","평범","적당","밸런스 좋다","깔끔", "담백", "청량","라이트","경쾌","가뿐","맑다","밸런스"));
-        bodyKeywords.put("묵직해요", Arrays.asList("거칠", "자극적","풍부","묵직","무겁","진하다","강함", "진한", "무겁다","무거움","숙성감","무게감"));
+        bodyKeywords.put("부드러워요", Arrays.asList("부드", "목넘김 좋다","순하다", "매끄", "술술","마일드","편안","매끈","크리미","여운"));
+        bodyKeywords.put("깔끔해요", Arrays.asList("무난","평범","적당","깔끔", "담백", "청량","라이트","경쾌","가뿐","맑다","밸런스"));
+        bodyKeywords.put("묵직해요", Arrays.asList("거칠", "자극적","풍부","묵직","진하다","강함", "진한","무거움","숙성감","무게감"));
         bodyKeywords.put("가벼워요", Arrays.asList("가볍", "산뜻","가벼움","후레쉬","도수가 약","약주"));
     }
 
@@ -67,8 +68,7 @@ public class LoadKeywordAnalyzer {
      * 가성비 키워드
      */
     private void loadValueKeywords() {
-        valueKeywords.put("가성비가 좋아요", Arrays.asList("가성비", "저렴", "가격대비 좋다", "합리적", "부담 없는",
-                            "재구매", "자주 구매", "합리적인 가격", "가격 만족","낮음"));
+        valueKeywords.put("가성비가 좋아요", Arrays.asList("가성비", "저렴", "합리적", "부담 없는", "재구매", "합리적인","낮음"));
         valueKeywords.put("가성비가 나빠요", Arrays.asList("비싸", "가격이 아깝다"));
     }
 
@@ -77,7 +77,7 @@ public class LoadKeywordAnalyzer {
      */
     private void loadSatisfactionKeywords() {
         satisfactionKeywords.put("추천해요", Arrays.asList("만족", "좋다", "또 마시고 싶다", "추천", "선물", "포장상태 좋은",
-                "기대 이상", "입문자 추천", "재구매", "최고","맘에드네요","굳","기대","맛있","짱","편리","좋았"));
+                "기대 이상", "입문자 추천", "재구매", "최고","맘에드네요","굳","기대","맛있","짱","편리","좋았","맛있다"));
         satisfactionKeywords.put("보통이에요", Arrays.asList("그닥","아쉽", "별로", "나쁘진 않은","괜찮","에매","다르","다른","그럭저럭","개성은 없"));
         satisfactionKeywords.put("별로에요", Arrays.asList("불만족","실망","최악","상한"));
     }
@@ -115,6 +115,7 @@ public class LoadKeywordAnalyzer {
         // 형태소 원형 추출
         List<String> morphemes = new ArrayList<>();
         for (Token token : tokens) {
+            if (STOPWORDS.contains(token.getMorph())) continue; // 감탄사 제외
             String pos = token.getPos(); // 품사
             String morph = token.getMorph(); // 형태소 원형
 
